422fbaeb82e3fc56de992edefd2e918166052a7c
MeraryMC <m_martinez_cobian@berkeley.edu>
2019-04-26 07:02:58 -0700

    added app, header, order, and inventory components

---
+++ catch-of-the-day/build/precache-manifest.aa0aadc1cd440d12aa95f09f8807634a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "a402cf6132ba376037ad",
+    "url": "/static/css/main.1aa83050.chunk.css"
+  },
+  {
+    "revision": "a402cf6132ba376037ad",
+    "url": "/static/js/main.a402cf61.chunk.js"
+  },
+  {
+    "revision": "0b76e2b518e8faff3f83",
+    "url": "/static/js/1.0b76e2b5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4c3fb584d3f63821357c8a9cff97f360",
+    "url": "/static/media/haymaker-webfont.4c3fb584.eot"
+  },
+  {
+    "revision": "d2f8799a13e6db2022d9a7de22776705",
+    "url": "/static/media/anchor.d2f8799a.svg"
+  },
+  {
+    "revision": "73a576a8254aeafebcdc66fe9fd41ac8",
+    "url": "/static/media/blanch_caps_inline-webfont.73a576a8.eot"
+  },
+  {
+    "revision": "cf798e30eda73d84b25c4ae233fe5e21",
+    "url": "/static/media/haymaker-webfont.cf798e30.svg"
+  },
+  {
+    "revision": "751e56274d3ea1c651ea824f902bb987",
+    "url": "/static/media/haymaker-webfont.751e5627.woff"
+  },
+  {
+    "revision": "1461f8e9d2084c70c00830348efe1766",
+    "url": "/static/media/blanch_caps_inline-webfont.1461f8e9.woff"
+  },
+  {
+    "revision": "e9535f20ca175cc3fdfe2816f7c47168",
+    "url": "/static/media/haymaker-webfont.e9535f20.ttf"
+  },
+  {
+    "revision": "8345e1b3362c1d640f0f11b5aae1d5bc",
+    "url": "/static/media/blanch_caps_inline-webfont.8345e1b3.ttf"
+  },
+  {
+    "revision": "7bbffda6bcacac8360f0ee00bb03dc04",
+    "url": "/static/media/blanch_caps_inline-webfont.7bbffda6.svg"
+  },
+  {
+    "revision": "4c0fa2f1bf9870120bb26dac8cd67518",
+    "url": "/index.html"
+  }
+];
